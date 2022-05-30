265b10758aee855b3197a76f6b89b7e89355dcd4
Kevin Müller <kevin.mueller1@outlook.com>
2022-05-30 17:36:11 +0200

    Add project files.

---
+++ GoogleOAuth2/wwwroot/googleOAuth2.js
@@ -0,0 +1,21 @@
+﻿window.googleOAuth2 = {
+    dotnetReference: undefined,
+    client: undefined,
+    access_token: undefined,
+    init: function (dotnetReference, clientId, scopes) {
+        window.googleOAuth2.dotnetReference = dotnetReference;
+
+        window.googleOAuth2.client = google.accounts.oauth2.initTokenClient({
+            client_id: clientId,
+            scope: 'https://www.googleapis.com/auth/books',
+            callback: (tokenResponse) => {
+                access_token = tokenResponse.access_token;
+                window.googleOAuth2.dotnetReference.invokeMethodAsync('OnGoogleAuthCallback', tokenResponse.access_token).then(_ => { });
+            },
+
+        });
+    },
+    requestToken: function () {
+        window.googleOAuth2.client.requestAccessToken();
+    }
+};
