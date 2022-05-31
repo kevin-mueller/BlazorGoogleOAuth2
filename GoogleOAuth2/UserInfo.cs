9412813ab5fab962970e0b8b973b1dc00b44e5a8
Kevin Müller <kevin.mueller1@outlook.com>
2022-05-31 13:24:29 +0200

    session handling + logout

---
+++ GoogleOAuth2/UserInfo.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleOAuth2
+{
+    public class UserInfo
+    {
+        [JsonProperty("sub")]
+        public string? Sub { get; set; }
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+        [JsonProperty("given_name")]
+        public string? GivenName { get; set; }
+        [JsonProperty("family_name")]
+        public string? FamilyName { get; set; }
+        [JsonProperty("picture")]
+        public string? Picture { get; set; }
+        [JsonProperty("email")]
+        public string? Email { get; set; }
+        [JsonProperty("email_verified")]
+        public bool EmailVerified { get; set; }
+        [JsonProperty("locale")]
+        public string? Locale { get; set; }
+        [JsonProperty("hd")]
+        public string? Hd { get; set; }
+    }
+
+}
